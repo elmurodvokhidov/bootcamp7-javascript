19511215003eb40e5d29cc2d0043269daae8f7c6
elmurodvokhidov <elmurodvkh@gmail.com>
2024-03-25 21:52:37 +0500

    lesson3

---
+++ main.js
@@ -0,0 +1,38 @@
+// let a = true; // 1
+// let b = false; // 0
+
+// console.log(a / b);
+
+// Boolean1
+// let a = -15;
+// console.log(a > 0);
+
+// Boolean2
+// let a = 22;
+// console.log(a % 2 != 0);
+
+// Boolean3
+// let a = 25;
+// console.log(a % 2 == 0);
+
+// Boolean4
+// let a = 34, b = 2;
+// console.log(a > 2 && b <= 3);
+
+// Boolean10
+// let a = 5, b = 4;
+// let x = a % 2 != 0 && b % 2 == 0;
+// let y = a % 2 == 0 && b % 2 != 0;
+// console.log(x || y);
+
+// Boolean25
+// let x = 2, y = -5;
+// let z = x < 0 && y > 0;
+// console.log(z);
+
+
+// console.log(5 > 2 || 3 > 5);
+
+// console.log(!false);
+
+// console.log(3 > 2 > 1);
