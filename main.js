37eaabb5eada8e57151f8832db51980b01fe026f
elmurodvokhidov <elmurodvkh@gmail.com>
2024-03-25 22:01:22 +0500

    lesson10

---
+++ main.js
@@ -0,0 +1,15 @@
+// let b = ['elmurod', 20, 'developer'];
+let a = {
+    ism: 'elmurod',
+    yosh: 20,
+    kasbi: 'developer',
+    links: {
+        telegram: 'elmurod@go.com',
+        instagram: 'elmurod@inst.com',
+    },
+};
+
+// console.log(a.yosh);
+// console.log(a['yosh']);
+
+console.log(a.links.telegram);
