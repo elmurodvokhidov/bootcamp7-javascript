fa97c5bf90bcd58ed011b85299c9faef40868214
elmurodvokhidov <elmurodvkh@gmail.com>
2024-03-25 22:05:24 +0500

    lesson12

---
+++ README.md
@@ -0,0 +1,1 @@
+# lesson12 - DOM, JS selectors and style elements from JS
---
+++ main.js
@@ -0,0 +1,67 @@
+// let h1 = document.getElementsByTagName('h1');
+// let ism = document.getElementsByName('ism');
+// let test = document.getElementsByClassName('test');
+// let test2 = document.getElementById('test2');
+// let tests = document.getElementsByClassName('test');
+
+// let h1 = document.querySelector('.h1');
+// let h1 = document.querySelectorAll('h1');
+
+
+// tests.forEach(test => {
+//     console.log(test);
+// });
+
+// let foo = h1[0].textContent += ' Yangi text!'
+
+// console.log(h1);
+
+
+
+
+// let foo = document.querySelectorAll('#foo');
+
+// foo.forEach((element) => {
+//     element.textContent += ' World!'
+// })
+
+// console.log(foo);
+
+// let h1 = document.querySelector('#foo');
+// let a = foo.textContent = `<p>yangi element</p>`;
+
+// let id = h1.getAttribute('id');
+
+// let class1 = h1.setAttribute('class', 'yangiClass');
+
+// h1.setAttribute('style', 'color: red');
+
+// console.log(h1.style);
+
+
+
+// const anchor = document.querySelector('a');
+// anchor.setAttribute('href', 'https://yandex.ru');
+
+// const box = document.querySelectorAll('.box');
+
+// box[0].style.width = '400px';
+// box[0].style.height = '400px';
+// box[0].style.backgroundColor = 'red';
+// box[0].style.color = 'blue';
+// box[0].style.margin = '50px';
+
+// box[0].innerHTML = `<img src="https://www.w3schools.com/images/img_fullaccess_down_sep3_pink_160.png" alt="">`
+// box[0].innerHTML += `<h1>Hello Title</h1>`
+
+
+
+
+// box[1].style.width = '400px';
+// box[1].style.height = '400px';
+// box[1].style.backgroundColor = 'red';
+
+// box[1].innerHTML = `<img src="https://www.w3schools.com/images/img_fullaccess_down_sep3_pink_160.png" alt="">`
+// box[1].innerHTML += `<h1>Hello Title</h1>`
+
+// console.log(box[0].style);
