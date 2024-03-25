9b9dc068a6bbfa160c09687d30ba3621455ab096
elmurodvokhidov <elmurodvkh@gmail.com>
2024-03-25 21:51:18 +0500

    lesson2

---
+++ README.md
@@ -0,0 +1,1 @@
+# lesson2 - parseInt, typeof, integer
---
+++ main.js
@@ -0,0 +1,33 @@
+// Integer1
+// let L = 100;
+// let n = L / 100;
+// console.log("Javob " + n + " metr");
+
+// Integer4
+// let a = 5, b = 2;
+// let n = a / b;
+// console.log(a + " ning ichida " + b + " ni " + n + " marta joylashtirish mumkin.");
+
+// Integer6
+// let x = 25;
+// let a = parseInt(x / 10) * 10;
+// let b = x % 10;
+// console.log(x + " ning o'nliklar xonasi " + a + " ga, birliklar xonasi " + b + " ga teng.");
+
+// alert("Bu alert xabari!")
+
+// let a = "Hello world!";
+
+// let b = typeof a;
+
+// console.log(b);
+
+// a = 5
+// b = 10
+// console.log(a + b);
+
+// Integer8
+// let x = 25;
+// let a = parseInt(x / 10);
+// let b = x % 10 * 10;
+// console.log(a + b);
